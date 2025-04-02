a75677d36f703e66c488d4190c3ee53b9b58ee57
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-04-02 11:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+	event.preventDefault();
+ let firstName = document.querySelector('#fname').value.trim();
+  let lastName = document.querySelector('#lname').value.trim();
+	  if (!firstName || !lastName) {
+                alert("Please enter both first and last names.");
+                return;
+            }
+            
+            alert(firstName + " " + lastName);
+        }
+        
+        document.querySelector('#nameForm').addEventListener('submit', getFormvalue);
+    
 
 }
